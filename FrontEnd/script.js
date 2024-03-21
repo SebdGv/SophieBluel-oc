b38d9748ddf8c983a8dd00fc8b828d5791b51587
SebdGv <bes.dgv@gmail.com>
2024-03-21 13:27:50 +0100

    add filters button

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -21,3 +21,38 @@ async function getProjects() {
   }
 }
 getProjects();
+
+// ===== Buttons filters  ===== //
+
+const sectionFilters = document.querySelector(".btn-filters");
+
+const btnAll = document.createElement("button");
+const btnObjects = document.createElement("button");
+const btnApparts = document.createElement("button");
+const btnHotels = document.createElement("button");
+
+btnAll.innerText = "Tous";
+btnObjects.innerText = "Objets";
+btnApparts.innerText = "Appartements";
+btnHotels.innerText = "Hôtels & restaurants";
+
+sectionFilters.appendChild(btnAll);
+sectionFilters.appendChild(btnObjects);
+sectionFilters.appendChild(btnApparts);
+sectionFilters.appendChild(btnHotels);
+
+const btnsFilters = document.querySelectorAll("button");
+for (let i = 0; i < btnsFilters.length; i++) {
+  const btn = btnsFilters[i];
+  btn.addEventListener("click", (e) => {
+    if (e.target === btnObjects) {
+      console.log("objets");
+    } else if (e.target === btnApparts) {
+      console.log("Appartements");
+    } else if (e.target === btnHotels) {
+      console.log("hotels & restaurants");
+    } else {
+      console.log("all");
+    }
+  });
+}

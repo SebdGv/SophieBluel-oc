717a00bb17de9311f519f79cc52dca44749b378b
SebdGv <bes.dgv@gmail.com>
2024-03-21 15:13:41 +0100

    add zoom img:hover

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -2,57 +2,62 @@ async function getProjects() {
   const reponse = await fetch("http://localhost:5678/api/works");
   const projects = await reponse.json();
 
-  for (let i = 0; i < projects.length; i++) {
-    const project = projects[i];
+  const generateCards = () => {
+    for (let i = 0; i < projects.length; i++) {
+      const project = projects[i];
 
-    const sectionProjects = document.querySelector(".gallery");
+      const sectionProjects = document.querySelector(".gallery");
 
-    const viewProject = document.createElement("figure");
-    const imageProject = document.createElement("img");
+      const viewProject = document.createElement("figure");
 
-    const captionProject = document.createElement("figcaption");
+      const imageZoom = document.createElement("div");
+      const imageProject = document.createElement("img");
+      const captionProject = document.createElement("figcaption");
 
-    imageProject.src = project.imageUrl;
-    captionProject.innerText = project.title;
+      imageZoom.classList.add("zoom");
+      imageProject.src = project.imageUrl;
+      captionProject.innerText = project.title;
 
-    sectionProjects.appendChild(viewProject);
-    viewProject.appendChild(imageProject);
-    viewProject.appendChild(captionProject);
+      sectionProjects.appendChild(viewProject);
+      viewProject.appendChild(imageZoom);
+      imageZoom.appendChild(imageProject);
+      viewProject.appendChild(captionProject);
+    }
+  };
+
+  // ===== Buttons filters  ===== //
+
+  const sectionFilters = document.querySelector(".btn-filters");
+
+  const btnAll = document.createElement("button");
+  const btnObjects = document.createElement("button");
+  const btnApparts = document.createElement("button");
+  const btnHotels = document.createElement("button");
+
+  btnAll.innerText = "Tous";
+  btnObjects.innerText = "Objets";
+  btnApparts.innerText = "Appartements";
+  btnHotels.innerText = "Hôtels & restaurants";
+
+  sectionFilters.appendChild(btnAll);
+  sectionFilters.appendChild(btnObjects);
+  sectionFilters.appendChild(btnApparts);
+  sectionFilters.appendChild(btnHotels);
+
+  const btnsFilters = document.querySelectorAll("button");
+  for (let i = 0; i < btnsFilters.length; i++) {
+    const btn = btnsFilters[i];
+    btn.addEventListener("click", (e) => {
+      if (e.target === btnObjects) {
+        console.log("Objets");
+      } else if (e.target === btnApparts) {
+        console.log("Appartements");
+      } else if (e.target === btnHotels) {
+        console.log("hotels & restaurants");
+      } else {
+      }
+    });
   }
+  generateCards();
 }
 getProjects();
-
-// ===== Buttons filters  ===== //
-
-const sectionFilters = document.querySelector(".btn-filters");
-
-const btnAll = document.createElement("button");
-const btnObjects = document.createElement("button");
-const btnApparts = document.createElement("button");
-const btnHotels = document.createElement("button");
-
-btnAll.innerText = "Tous";
-btnObjects.innerText = "Objets";
-btnApparts.innerText = "Appartements";
-btnHotels.innerText = "Hôtels & restaurants";
-
-sectionFilters.appendChild(btnAll);
-sectionFilters.appendChild(btnObjects);
-sectionFilters.appendChild(btnApparts);
-sectionFilters.appendChild(btnHotels);
-
-const btnsFilters = document.querySelectorAll("button");
-for (let i = 0; i < btnsFilters.length; i++) {
-  const btn = btnsFilters[i];
-  btn.addEventListener("click", (e) => {
-    if (e.target === btnObjects) {
-      console.log("objets");
-    } else if (e.target === btnApparts) {
-      console.log("Appartements");
-    } else if (e.target === btnHotels) {
-      console.log("hotels & restaurants");
-    } else {
-      console.log("all");
-    }
-  });
-}
